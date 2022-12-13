de207b87acd1520c319f57a2253cb8c86140f38a
Reginaldo Santos <reginaldo.santos@redspace.com>
2022-12-12 23:02:15 -0400

    test: ensure return the sum of the integers in the matrix on success

--- spec/tests/service/csv-reader.test.ts
+++ spec/tests/service/csv-reader.test.ts
@@ -53,4 +53,31 @@ describe('CSV Reader service', () => {
 			expect(result).toBe(fileData)
 		})
 	})
+
+	describe('sum', () => {
+		it('should throw error when no data is provided', async () => {
+			const reader = new CSVReader()
+
+			await expect(reader.sum(null as any)).rejects.toThrow(
+				new Error('Invalid data')
+			)
+		})
+
+		it('should throw error when no data is not an array', async () => {
+			const reader = new CSVReader()
+
+			await expect(reader.sum('1, 2, 3' as any)).rejects.toThrow(
+				new Error('Invalid data')
+			)
+		})
+
+		it('should return the matrix as a 1 line string, with values separated by commas on success', async () => {
+			const fileMock = path.resolve(__dirname, 'mock.csv')
+			const reader = new CSVReader()
+			const data = await reader.read(fileMock)
+			const result = await reader.sum(data)
+
+			expect(result).toBe(45)
+		})
+	})
 })
