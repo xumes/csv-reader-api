3d4df4445de16344b074d65baad9de913066bb5f
Reginaldo Santos <reginaldo.santos@redspace.com>
2022-12-14 21:16:55 -0400

    feat: create route to flatten the csv data

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import echo from './echo'
 import invert from './invert'
+import flatten from './flatten'
 
 // Export the base-router
 const baseRouter = Router()
@@ -8,6 +9,7 @@ const baseRouter = Router()
 // Setup routers
 baseRouter.use('/echo', echo)
 baseRouter.use('/invert', invert)
+baseRouter.use('/flatten', flatten)
 
 // Export default.
 export default baseRouter
