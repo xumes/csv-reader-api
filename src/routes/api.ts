5995b0cc9640d885acfc78785a4610ba04c98b8b
Reginaldo Santos <reginaldo.santos@redspace.com>
2022-12-14 21:11:41 -0400

    feat: create route to invert the csv data

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 import echo from './echo'
+import invert from './invert'
 
 // Export the base-router
 const baseRouter = Router()
 
 // Setup routers
 baseRouter.use('/echo', echo)
+baseRouter.use('/invert', invert)
 
 // Export default.
 export default baseRouter
---
+++ src/routes/invert.ts
@@ -0,0 +1,44 @@
+import { Router } from 'express'
+import multer from 'multer'
+import os from 'os'
+import CSVReader from '../services/csv-reader'
+import { HttpStatusCode } from '../@shared/http-status-code'
+import { ParamMissingError } from '../@shared/errors'
+
+const router = Router()
+
+const upload = multer({ dest: os.tmpdir() })
+// Setup routers
+router.use('/', upload.single('file'), async (req, res) => {
+	const { file } = req
+
+	if (!file) {
+		res.status(HttpStatusCode.BAD_REQUEST).json({
+			error: 'File not found',
+		})
+		return
+	}
+
+	try {
+		const data = await CSVReader.read(file.path)
+
+		if (!CSVReader.isValid(data)) {
+			res.status(HttpStatusCode.BAD_REQUEST).json({
+				error: 'Invalid data',
+			})
+		}
+
+		const inverted = await CSVReader.invert(data)
+
+		res.status(HttpStatusCode.OK).send(CSVReader.format(inverted))
+	} catch (err) {
+		if (err instanceof ParamMissingError) {
+			res.status(HttpStatusCode.BAD_REQUEST).json({
+				error: err.message,
+			})
+		}
+	}
+})
+
+// Export default.
+export default router
